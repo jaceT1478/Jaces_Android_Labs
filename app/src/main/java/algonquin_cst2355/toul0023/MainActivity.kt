43b547a188258239c88a8925649c2eb1c3edbf53
jaceT1478 <120132221+jaceT1478@users.noreply.github.com>
2023-10-03 16:15:04 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin_cst2355/toul0023/MainActivity.kt
+++ app/src/main/java/algonquin_cst2355/toul0023/MainActivity.kt
@@ -1,11 +1,49 @@
 package algonquin_cst2355.toul0023
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.CompoundButton
+import android.widget.ImageView
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imgView: ImageView
+    lateinit var sw: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imgView = findViewById(R.id.imageView3);
+        sw = findViewById(R.id.switch1);
+
+        sw.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { btn: CompoundButton?, isChecked: Boolean ->
+
+            if (isChecked) {
+                val rotate = RotateAnimation(
+                    0f,
+                    360f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f
+                )
+                rotate.duration = 5000
+                rotate.repeatCount = Animation.INFINITE
+                rotate.interpolator = LinearInterpolator()
+                imgView.startAnimation(rotate)
+
+            }
+
+            else {
+                imgView.clearAnimation();
+            }
+        })
+
+
     }
 }
